0a4f863554f5538296e2aef9ff0a22acd09c5eee
Lars Gimse <lagi@iktsenteret.no>
2017-10-19 14:11:31 +0200

    Create boy_girl.py

---
+++ python/boy_girl.py
@@ -0,0 +1,20 @@
+# gutt=boy and jente=girl - most popular boy ang girl name in Norway 2016
+
+from microbit import *
+import random
+
+gutt = """William,Oskar,Lucas,Mathias,Filip,Oliver,Jakob,Emil,Noah,Aksel"""
+jente = """Nora,Emma,Sara,Sofie,Sofia,Maja,Olivia,Ella,Ingrid,Emilie"""
+            
+gutt_split = gutt.split(",", )
+jente_split = jente.split(",", )
+
+while True:
+    if button_a.is_pressed():
+        display.scroll(random.choice(gutt_split))
+    if button_b.is_pressed():
+        display.scroll(random.choice(jente_split))
+    if pin0.is_touched():
+         display.scroll(random.choice(gutt_split + jente_split))
+    else:
+        display.show(Image.HEART)
